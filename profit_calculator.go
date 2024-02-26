d9da72ea6ac3c2c3232c20f39be037ae4ce61a6a
= <=>
2024-02-26 16:18:37 +0700

    make function

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,24 +3,27 @@ package main
 import "fmt"
 
 func main(){
-	var revenue float64
-	var expenses float64
-	var taxRate float64 
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	ebt, profit, ratio := calculateFinancial(revenue, expenses, taxRate)
 
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	fmt.Printf("%.1f\n",ebt)
+	fmt.Printf("%.1f\n",profit)
+	fmt.Printf("%.3f\n",ratio)
+}
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
+func calculateFinancial(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
+	return ebt, profit, ratio
+}
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
